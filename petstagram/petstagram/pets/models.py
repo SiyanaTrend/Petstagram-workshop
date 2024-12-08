62b0614368ee9a532d8bf80adda8e509d4f6e4f1
Siyana Trendafilova <bebovska@gmail.com>
2024-12-08 19:01:41 +0200

    fix: Fixed slug field in pet model

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -6,9 +6,11 @@ class Pet(models.Model):
     name = models.CharField(max_length=30)
     personal_photo = models.URLField()
     date_of_birth = models.DateField(blank=True, null=True)
-    slug = models.SlugField(blank=True, null=False, unique=True)
+    slug = models.SlugField(blank=True, null=False, unique=True, editable=False)
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
         if not self.slug:
             self.slug = slugify(f"{self.name}-{self.id}")
 
