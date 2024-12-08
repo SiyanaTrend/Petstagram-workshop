76ae1316497a40b963804f0449de87bef7ea02ac
Siyana Trendafilova <bebovska@gmail.com>
2024-12-08 16:51:04 +0200

    feat: Added 4 apps with their views and urls

--- petstagram/petstagram/photos/views.py
+++ petstagram/petstagram/photos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def photo_add(request):
+    return render(request, 'photos/photo-add-page.html')
+
+def photo_details(request, pk: int):
+    return render(request, 'photos/photo-details-page.html')
+
+def photo_edit(request, pk: int):
+    return render(request, 'photos/photo-edit-page.html')
