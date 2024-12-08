aed15c46a96ad238c27f4637ad00904475e34949
Siyana Trendafilova <bebovska@gmail.com>
2024-12-08 19:43:37 +0200

    feat: Added photo model and validator

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -3,10 +3,21 @@ from django.db import models
 
 
 class Pet(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(
+        max_length=30,
+    )
     personal_photo = models.URLField()
-    date_of_birth = models.DateField(blank=True, null=True)
-    slug = models.SlugField(blank=True, null=False, unique=True, editable=False)
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+    slug = models.SlugField(
+        blank=True,
+        null=False,
+        unique=True,
+        editable=False,
+    )
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
