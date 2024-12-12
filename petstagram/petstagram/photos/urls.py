7b830aa33510443b5f950eb22b3253f63122c3c2
Siyana Trendafilova <bebovska@gmail.com>
2024-12-12 14:57:02 +0200

    feat: Created template inheritance

--- petstagram/petstagram/photos/urls.py
+++ petstagram/petstagram/photos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from petstagram.photos import views
 
 urlpatterns = [
-    path('add/', views.photo_add, name='photo-add'),
+    path('add/', views.photo_add, name='add-photo'),
     path('<int:pk>/', include([
         path('', views.photo_details, name='photo-details'),
         path('edit/', views.photo_edit, name='photo-edit'),
