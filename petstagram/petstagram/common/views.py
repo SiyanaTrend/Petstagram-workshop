8fccb542389e617638629d307b44027802b582a4
Siyana Trendafilova <bebovska@gmail.com>
2024-12-12 17:57:09 +0200

    feat: Added like button functionality
    see common/views.py

--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.common.models import Like
 from petstagram.photos.models import Photo
 
 
@@ -10,3 +11,22 @@ def home_page(request):
         'all_photos': all_photos,
     }
     return render(request, 'common/home-page.html', context)
+
+
+def like_functionality(request, photo_id: int):
+    liked_object = Like.objects.filter(
+        to_photo_id=photo_id,
+    ).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo_id=photo_id)
+        like.save()
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
+    # when a fragment is added -> ... + f'#{photo_id}'
+    # => when the heart button is clicked, the page stays on the liked post
+
+    # return redirect(request.META.get('HTTP_REFERER'))
+    # => when the heart button is clicked, the page returns at the beginning
